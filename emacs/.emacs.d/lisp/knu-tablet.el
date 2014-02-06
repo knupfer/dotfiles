c7cfcc53a8f925ca41e710df5469f055a98ba4e9
Knupfer <fknupfer@gmail.com>
2014-02-06 12:59:43 +0100

    Auto hide empty echo area

--- emacs/.emacs.d/lisp/knu-tablet.el
+++ emacs/.emacs.d/lisp/knu-tablet.el
@@ -1,7 +1,9 @@
 (setq default-frame-alist (append default-frame-alist '((minibuffer . nil))))
 (setq minibuffer-auto-raise t)
 (setq minibuffer-frame-alist (quote ((width . 150) (height . 2) (background-color . "#303") (foreground-color . "#eee"))))
-(run-with-idle-timer 1 t 'raise-frame)
+
+(run-with-idle-timer 0.5 t '(lambda () (if (equal (current-message) nil) (raise-frame) ())))
+
 (run-with-idle-timer 1 nil 'select-frame-by-name "emacs@localhost")
 
 (add-hook 'exit-minibuffer-hook 'raise-frame)
