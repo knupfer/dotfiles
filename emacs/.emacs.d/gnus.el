653be6b67db68dfeef7a4efd300a3b2189ff0b82
Knupfer <fknupfer@gmail.com>
2014-02-06 22:17:57 +0100

    Show the date of the mails

--- emacs/.emacs.d/gnus.el
+++ emacs/.emacs.d/gnus.el
@@ -11,3 +11,42 @@
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
       starttls-use-gnutls t)
+
+(setq gnus-thread-sort-functions                                                           
+      '((not gnus-thread-sort-by-date)
+        (not gnus-thread-sort-by-number)))
+
+(setq-default
+  gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f  %B%s%)\n"
+  gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
+  gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
+  gnus-sum-thread-tree-false-root ""
+  gnus-sum-thread-tree-indent ""
+  gnus-sum-thread-tree-leaf-with-other "-> "
+  gnus-sum-thread-tree-root ""
+  gnus-sum-thread-tree-single-leaf "|_ "
+  gnus-sum-thread-tree-vertical "|")
+
+
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+
+(setq gnus-summary-thread-gathering-function
+      'gnus-gather-threads-by-subject)
+
+(setq user-full-name "Florian Knupfer"
+      user-mail-address "fknupfer@gmail.com"
+      message-generate-headers-first t
+      )
+
+;(setq gnus-posting-styles
+;      '((".*"
+;     (name "My Name"
+;          (address "username@gmail.com"
+;                   (organization "")
+;                   (signature-file "~/.signature")
+;                   ("X-Troll" "Emacs is better than Vi")
+;                   )))))
+
+(setq mm-text-html-renderer 'w3m)
+
+;sehr gutes Nachschlagewerk:  http://blog.binchen.org/?p=403
