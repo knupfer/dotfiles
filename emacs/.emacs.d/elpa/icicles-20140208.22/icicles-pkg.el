7e24f757f61444ab060f0bc8382e2fc363a368d3
Knupfer <fknupfer@gmail.com>
2014-02-11 22:57:17 +0100

    Update packages

--- emacs/.emacs.d/elpa/icicles-20140208.22/icicles-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "icicles" "20140208.22" "Minibuffer input completion and cycling." 'nil)
---
+++ emacs/.emacs.d/elpa/icicles-20140211.737/icicles-pkg.el
@@ -0,0 +1,1 @@
+(define-package "icicles" "20140211.737" "Minibuffer input completion and cycling." 'nil)
