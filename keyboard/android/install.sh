6c1b450a74f3b50b981184859b78423fdea86ccf
Knupfer <fknupfer@gmail.com>
2014-02-17 18:23:16 +0100

    Android native adnw

---
+++ keyboard/android/install.sh
@@ -0,0 +1,4 @@
+cp Generic.kcm /system/usr/keychars/Generic.kcm
+cp Generic.kl /system/usr/keylayout/Generic.kl
+chmod 777 /system/usr/keychars/Generic.kcm
+chmod 777 /system/usr/keylayout/Generic.kl
