7a08c0b6a91a3eea48aa53f71e12b967e0fa0cb5
Florian Knupfer <fknupfer@gmail.com>
2014-04-06 20:50:45 +0200

    Support volume in emacs

---
+++ emacs/.emacs.d/elpa/volume-20121025.1538/volume-autoloads.el
@@ -0,0 +1,28 @@
+;;; volume-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (volume) "volume" "volume.el" (21313 32554 40000
+;;;;;;  4000))
+;;; Generated autoloads from volume.el
+
+(autoload 'volume "volume" "\
+Tweak your sound card volume.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("volume-pkg.el") (21313 32554 122308 3000))
+
+;;;***
+
+(provide 'volume-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; volume-autoloads.el ends here
---
+++ emacs/.emacs.d/elpa/volume-20121025.1538/volume-pkg.el
@@ -0,0 +1,1 @@
+(define-package "volume" "20121025.1538" "tweak your sound card volume from Emacs" (quote nil))
