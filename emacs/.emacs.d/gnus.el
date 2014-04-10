59edbf9c851f7aa1b9fbfd8f89cad9c2f328111f
Florian Knupfer <fknupfer@gmail.com>
2014-04-10 16:47:15 +0200

    Major cleanup and improvement
    - show always Gesendet
    - show read emails
    - use only 80 columns

--- emacs/.emacs.d/gnus.el
+++ emacs/.emacs.d/gnus.el
@@ -1,3 +1,7 @@
+(setq user-full-name "Florian Knupfer"
+      user-mail-address "fknupfer@gmail.com"
+      message-generate-headers-first t)
+
 (setq gnus-select-method
       '(nnimap "gmail"
                (nnimap-address "imap.gmail.com")
@@ -10,43 +14,33 @@
       smtpmail-default-smtp-server "smtp.gmail.com"
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
-      starttls-use-gnutls t)
-
-(setq gnus-thread-sort-functions                                                           
-      '((not gnus-thread-sort-by-date)
-        (not gnus-thread-sort-by-number)))
+      starttls-use-gnutls t
+      gnus-thread-sort-functions '((not gnus-thread-sort-by-date)))
 
 (setq-default
-  gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f  %B%s%)\n"
-  gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
+ gnus-summary-line-format "%(%&user-date;%* %-12,12a%R%O%B%-0,59s%)\n"
+  gnus-user-date-format-alist '((t . "%d-%m"))
   gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
   gnus-sum-thread-tree-false-root ""
-  gnus-sum-thread-tree-indent ""
-  gnus-sum-thread-tree-leaf-with-other "-> "
+  gnus-sum-thread-tree-indent " "
+  gnus-sum-thread-tree-leaf-with-other "->  "
   gnus-sum-thread-tree-root ""
-  gnus-sum-thread-tree-single-leaf "|_ "
+  gnus-sum-thread-tree-single-leaf "\\  "
   gnus-sum-thread-tree-vertical "|")
-
 
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
 (setq gnus-summary-thread-gathering-function
       'gnus-gather-threads-by-subject)
 
-(setq user-full-name "Florian Knupfer"
-      user-mail-address "fknupfer@gmail.com"
-      message-generate-headers-first t
-      )
-
-;(setq gnus-posting-styles
-;      '((".*"
-;     (name "My Name"
-;          (address "username@gmail.com"
-;                   (organization "")
-;                   (signature-file "~/.signature")
-;                   ("X-Troll" "Emacs is better than Vi")
-;                   )))))
-
-;(setq mm-text-html-renderer 'w3m)
-
 ;sehr gutes Nachschlagewerk:  http://blog.binchen.org/?p=403
+
+(setq gnus-permanently-visible-groups ".*Gesendet")
+
+(setq gnus-parameters
+      '(("INBOX"
+         (display . 41))
+        (".*Gesendet"
+         (display . all))))
+
+
