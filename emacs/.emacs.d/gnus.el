57afe72fcc63efbfa7f5adbfe3eaa789db478ceb
knupfer <fknupfer@gmail.com>
2014-09-01 22:19:31 +0200

    use ssl for usenet

--- emacs/.emacs.d/gnus.el
+++ emacs/.emacs.d/gnus.el
@@ -1,25 +1,13 @@
-(setq user-full-name "Florian Knupfer"
-      user-mail-address "fknupfer@gmail.com"
-      message-generate-headers-first t)
-
 (setq gnus-select-method
       '(nnimap "gmail"
                (nnimap-address "imap.gmail.com")
                (nnimap-server-port 993)
                (nnimap-stream ssl)))
 
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587
-                                   "fknupfer@gmail.com" nil))
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      starttls-use-gnutls t)
-
 (add-to-list 'gnus-secondary-select-methods
              '(nntp "eternal september"
-                    (nntp-address "news.eternal-september.org")))
+                    (nntp-address "reader443.eternal-september.org")
+                    (nntp-authinfo-force t)))
 
 (add-to-list 'gnus-secondary-select-methods
              '(nntp "gmane"
