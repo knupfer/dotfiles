e6561a330bd55f4f56a63194e0849023aa2105d9
knupfer <fknupfer@gmail.com>
2014-09-07 10:59:40 +0200

    remove dropbox

--- emacs/.emacs.d/elpa/dropbox-20130513.1537/dropbox-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; dropbox-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/elpa/dropbox-20130513.1537/dropbox-autoloads.el"
-;;;;;;  "../../../../../../.emacs.d/elpa/dropbox-20130513.1537/dropbox.el")
-;;;;;;  (21238 1698 883565 368000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dropbox-autoloads.el ends here
