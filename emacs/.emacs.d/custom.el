dd48d80a811fb3c1609de6acb3740b693358b65d
Florian Knupfer <fknupfer@gmail.com>
2014-12-07 12:06:04 +0100

    cleanup and use customize for some stuff

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
+ '(org-babel-load-languages
+   (quote
+    ((python . t)
+     (ditaa . t)
+     (sh . t)
+     (lilypond . t)
+     (R . t)
+     (emacs-lisp . t)
+     (ledger . t)
+     (haskell . t))))
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
