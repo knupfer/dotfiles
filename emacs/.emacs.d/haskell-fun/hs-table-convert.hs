3844febefa3860b7f2f2f64cbb3b3bdcb6082988
knupfer <fknupfer@gmail.com>
2014-12-13 15:53:23 +0100

    new compactified, fast table converter

--- emacs/.emacs.d/haskell-fun/hs-table-convert.hs
+++ emacs/.emacs.d/haskell-fun/hs-table-convert.hs
@@ -1,59 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.List (intersperse, transpose, foldl')
-import qualified Data.Text    as T
-import qualified Data.Text.IO as T (interact)
-import           Numeric (readFloat, readDec, readSigned, showGFloat)
+import           Control.Applicative
+import           Data.Attoparsec.Text
+import           Data.List
+import qualified Data.Text            as T
+import qualified Data.Text.IO         as T (interact)
 
 main :: IO ()
-main = T.interact parseCSV
+main = T.interact parseCsv
 
-parseCSV :: T.Text -> T.Text
-parseCSV = T.unlines .
-           init .
-           foldl' (\acc x -> if x/= head acc then x:acc else acc) [""] .
-           putPipes .
-           transpose .
-           align .
-           stripNum .
-           stripEmpty .
-           transpose .
-           splitDelimiter .
-           filter (not . T.null) .
-           foldl' (\acc x -> if x/= head acc then x:acc else acc) [""] .
-           T.lines
+parseCsv :: T.Text -> T.Text
+parseCsv = T.unlines   . putPipes          -- print result
+         . map head    . group . transpose -- remove duplicates
+         . align       . stripNum          -- prettify numbers
+         . filter (not . all T.null)       -- remove empty cols
+         . transpose   . splitDelimiter    -- transpose matrix
+         . filter (not . T.null) . T.lines -- remove empty rows
 
 putPipes :: [[T.Text]] -> [T.Text]
-putPipes = map (\t -> T.strip $ T.unwords $ intersperse "|" $ "":t ++ [""])
-
-stripEmpty :: [[T.Text]] -> [[T.Text]]
-stripEmpty = filter (not . all T.null)
+putPipes = map (\t -> T.unwords $ "|":intersperse "|" t ++ ["|"])
 
 align :: [[T.Text]] -> [[T.Text]]
-align = map (\n -> pad n (maximum $ map T.length n))
-  where pad n lim = map (T.justifyRight lim ' ') n
+align = map (\n -> pad (maximum $ map T.length n) n)
+  where pad lim = map (T.justifyRight lim ' ')
+
+parseNum :: Parser T.Text
+parseNum = ((\x -> if T.isSuffixOf ".0" x || T.isInfixOf ".0e" x
+                    then T.replace ".0" "" x
+                    else x) . T.pack . show)
+           <$> scientific <* endOfInput
 
 stripNum :: [[T.Text]] -> [[T.Text]]
-stripNum = map doStrip
-  where
-    doStrip m
-      | all (isStrNum (readSigned readDec :: String -> [(Int,String)])) m
-             = m
-      | all (isStrNum (readSigned readFloat :: String -> [(Float,String)])) m
-             = map (remNull . (\n -> showGFloat (Just 3) n "")
-                 . fst . head . (readSigned readFloat :: String -> [(Float,String)]) . T.unpack) m
-      | otherwise = m
-      where isStrNum fun x = not (null . fun . T.unpack $ x)
-             && ""== snd (head . fun . T.unpack $ x)
-            remNull str = let s = T.pack $ show (read str :: Double)
-                          in if T.isSuffixOf ".0" s
-                                then T.init $ T.init s
-                                else T.replace ".0e" "e" s
+stripNum = filter (any ("0"/=))
+         . map (map (\x -> either (const x) id $ parseOnly parseNum x))
 
 splitDelimiter :: [T.Text] -> [[T.Text]]
-splitDelimiter str
-  | all (T.any ('\t'==)) str = cells "\t"
-  | all (T.any (',' ==)) str = cells ","
-  | all (T.any (';' ==)) str = cells ";"
-  | all (T.any ('|' ==)) str = cells "|"
-  | otherwise                = cells " "
-  where cells c = map (map T.strip . T.splitOn c) str
+splitDelimiter s = maybe (error "Can't find a delimiter.") cells delim
+  where cells c  = map (map T.strip . T.split (c==)) s
+        delim    = find (\c -> all (T.any (c==)) s) "\t,;| "
