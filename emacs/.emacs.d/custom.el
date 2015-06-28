7933032aa3470d73ccdfbceee80dd835cf333391
knupfer <fknupfer@gmail.com>
2015-06-28 11:17:54 +0200

    clean up init

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(global-pretty-sha-path-mode t)
  '(indent-tabs-mode nil)
  '(org-babel-load-languages
    (quote
@@ -19,6 +20,8 @@
    (quote
     (oneonone hackernews yasnippet weather-metno w3m volume undo-tree tco smtpmail-multi smart-mode-line shm python-pep8 python-mode pymacs password-store paredit paradox package-build org-caldav oauth num3-mode multiple-cursors markdown-mode magit lorem-ipsum ledger-mode keyfreq jedi icicles htmlize hlinum hl-defined highlight-parentheses haskell-mode ghci-completion flyspell-lazy flycheck floobits fit-frame find-file-in-repository fill-column-indicator diff-hl dictionary crontab-mode company-ghc centered-cursor-mode browse-kill-ring bbdb-vcard autopair async ace-jump-mode)))
  '(paradox-github-token t)
+ '(pretty-sha-path-char 955)
+ '(pretty-sha-path-regexp "\\(/\\(?:gnu\\|nix\\)/store/[0-9a-df-np-sv-z]\\{32\\}\\)")
  '(truncate-lines t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
